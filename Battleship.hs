eeef77c2db87e00e06bd3c5d962d62952418b61d
Jonathan W. Cranford <jwcranford@ldschurch.org>
2016-11-29 16:52:25 -0700

    first working version to create standard board

---
+++ Battleship.hs
@@ -0,0 +1,62 @@
+-- Battleship game
+
+import Data.Ix (range)
+import System.Random
+import Data.Array
+
+data Ship = Ship { shipType:: Int, coords:: [(Int,Int)] } deriving Show
+
+data Orientation = Vertical | Horizontal deriving (Enum, Show)
+
+createShip :: Int -> Orientation -> (Int,Int) -> Ship
+createShip size Vertical begin@(x,y)   = Ship size $ range (begin, (x,y+size-1))
+createShip size Horizontal begin@(x,y) = Ship size $ range (begin, (x+size-1,y))
+
+orientationFromInt :: Int -> Orientation
+orientationFromInt x = toEnum $ x `mod` 2
+
+randomOrientation :: IO Orientation
+randomOrientation = do i <- getStdRandom random; return $ orientationFromInt i
+
+randomPlacement :: Int -> (Int, Int) -> Orientation -> IO (Int,Int)
+randomPlacement size (maxx, maxy) Vertical = 
+    do x <- getStdRandom $ randomR (1, maxx)
+       y <- getStdRandom $ randomR (1, maxy - size + 1)
+       return (x,y)
+randomPlacement size (maxx, maxy) Horizontal = 
+    do x <- getStdRandom $ randomR (1, maxx - size + 1)
+       y <- getStdRandom $ randomR (1, maxy)
+       return (x,y)
+       
+-- places a ship of given size randomly on a grid of the given bounds
+placeShipRandomly :: (Int, Int) -> Int -> IO Ship
+placeShipRandomly bnds size = 
+    do ori <- randomOrientation
+       p <- randomPlacement size bnds ori
+       return $ createShip size ori p
+
+placeStandardShipsRandomly :: IO [Ship]
+placeStandardShipsRandomly = sequence $ map (placeShipRandomly (10,10)) [2, 3, 3, 4, 5]
+
+data Cell = Vacant Bool | Occupied Ship Bool deriving Show
+
+data Board = Board { board :: Array (Int,Int) Cell
+                    , shipTotal :: Int
+                    , shipsSunk :: Int
+                    , ships :: [Ship] } 
+
+-- instance Show Board where
+    -- TODO implement show
+    -- . . x . . - hit
+    -- . . o . . - miss
+    -- . . X . . - hit ship
+    -- . . O . . - non-hit ship
+
+createStandardBoard :: [Ship] -> Board
+createStandardBoard [] = Board (array ((0,0),(0,0)) []) 0 0 []
+createStandardBoard ss = 
+    let assoc = [(cs, Occupied ship False) | ship <- ss, cs <- coords ship]
+    in Board { board = (accumArray (\_ a -> a) (Vacant False) ((1,1), (10,10)) assoc),
+               shipTotal = length ss,
+               shipsSunk = 0,
+               ships = ss }
