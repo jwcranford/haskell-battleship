bff02cc2dbfe30acc79d44ce974c92ae4ac6dbae
Jonathan W Cranford <jonathan.w.cranford@gmail.com>
2017-07-24 21:31:20 -0600

    Moved Ship to separate module

---
+++ src/Ship.hs
@@ -0,0 +1,50 @@
+module Ship where
+
+import Data.Ix 
+import System.Random
+
+type BoardIx = (Char,Char)
+
+data Ship = Ship { shipType:: String, numHits:: Int, coords:: [BoardIx] } deriving (Show, Eq)
+
+data Orientation = Vert | Horiz deriving (Enum, Show)
+
+createShip :: String -> Int -> Orientation -> BoardIx -> Ship
+createShip name size Horiz begin@(x,y) =
+  let y' = toEnum $ (fromEnum y) + size - 1
+  in Ship name size $ range (begin, (x,y'))
+createShip name size Vert begin@(x,y) = 
+  let x' = toEnum $ (fromEnum x) + size - 1
+  in Ship name size $ range (begin, (x',y))
+
+orientationFromInt :: Int -> Orientation
+orientationFromInt x = toEnum $ x `mod` 2
+
+randomOrientation :: IO Orientation
+randomOrientation = do i <- getStdRandom random; return $ orientationFromInt i
+
+standardBoardSize :: (BoardIx,BoardIx)
+standardBoardSize = (('a','0'), ('j','9'))
+
+randomPlacement :: Int -> (BoardIx,BoardIx) -> Orientation -> IO BoardIx
+randomPlacement size ((minx,miny),(maxx, maxy)) Horiz = 
+  let maxy' = toEnum $ (fromEnum maxy) - size + 1
+  in do x <- getStdRandom $ randomR (minx, maxx)
+        y <- getStdRandom $ randomR (miny, maxy')
+        return (x,y)
+randomPlacement size ((minx,miny),(maxx, maxy)) Vert = 
+  let maxx' = toEnum $ (fromEnum maxx) - size + 1
+  in do x <- getStdRandom $ randomR (minx, maxx')
+        y <- getStdRandom $ randomR (miny, maxy)
+        return (x,y)
+       
+-- places a ship of given size randomly on a grid of the given bounds
+placeShipRandomly :: (BoardIx,BoardIx) -> (String, Int) -> IO Ship
+placeShipRandomly bnds (name, size) = 
+    do ori <- randomOrientation
+       p <- randomPlacement size bnds ori
+       return $ createShip name size ori p
+
+placeStandardShipsRandomly :: IO [Ship]
+placeStandardShipsRandomly = 
+    sequence $ map (placeShipRandomly standardBoardSize) [("Destroyer",2), ("Cruiser",3), ("Submarine",3), ("Battleship",4), ("Carrier",5)]
