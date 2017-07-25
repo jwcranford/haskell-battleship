99d71f236ef22d1a9db65e7f6eaded6fbc1402ce
Jonathan W Cranford <jonathan.w.cranford@gmail.com>
2017-07-24 22:30:07 -0600

    moved Cell to separate module

--- src/Battleship.hs
+++ src/Battleship.hs
@@ -13,40 +13,9 @@ module Battleship (Board(..)
   ) where
 
 import Data.Array
-import Control.Monad
 import Ship
+import Cell
 
-
-data Cell = Vacant (Maybe Bool) 
-        | Occupied Ship Bool 
-        | Collision Ship Ship
-        deriving Eq
-
-instance Show Cell where
-    show (Vacant Nothing)      = "."
-    show (Vacant (Just True))  = "x"
-    show (Vacant (Just False)) = "*"
-    show (Occupied _ True)     = "X"
-    show (Occupied _ False)    = "O"
-    show (Collision _ _)       = "#"
-
--- Cells can be combined as long as at least one is Vacant
-instance Monoid Cell where
-    mempty = Vacant Nothing
-
-    mappend (Vacant ma) (Vacant mb) =
-        Vacant $ liftM2 (\a b -> or [a, b]) ma mb
-    mappend (Vacant _) occ@(Occupied _ _) = occ
-    mappend occ@(Occupied _ _) (Vacant _) = occ
-    mappend (Occupied s1 _) (Occupied s2 _) = Collision s1 s2
-    mappend c@(Collision _ _) _ = c
-    mappend _ c@(Collision _ _) = c
-
--- simple predicate for whether a cell is a collision or not
-collision :: Cell -> Bool
-collision (Collision _ _) = True
-collision _ = False
-    
 data Board = Board { board :: Array BoardIx Cell
                     , shipTotal :: Int
                     , shipsSunk :: Int
