2fb76794d624cac6ae3a01436ba27daf8b804dfd
Jonathan W Cranford <jonathan.w.cranford@gmail.com>
2017-08-12 21:03:56 -0600

    Changed main to keep real and shadow board
    Now uses Map instead of sequence, with random number as the key, for
    better concurrent behavior.
    
    Returns the shadow board to the client, keeps the real board in memory

--- src/SolitaireScotty.hs
+++ src/SolitaireScotty.hs
@@ -1,35 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
+import Battleship (Board,
+                   createValidStandardRandomBoard,
+                   emptyStandardBoard)
+
 import Data.IORef (newIORef, atomicModifyIORef', readIORef)
-import Control.Monad.Trans (lift, liftIO)
+import qualified Data.Map.Strict as Map (empty, lookup, insert)
+
+-- ghc
 import GHC.Generics (Generic)
+
+-- mtl
+import Control.Monad.Trans (liftIO)
+
+-- scotty
 import Web.Scotty (scotty, post, get, param, json, status)
 import Network.HTTP.Types.Status (notFound404)
+
+-- aeson
 import Data.Aeson (ToJSON)
-import Battleship (Board, createValidStandardRandomBoard)
-import Data.Sequence (empty, (|>), (!?))
 
-data Game = Game { handle :: Int, board :: Board } deriving Generic
+-- random
+import System.Random (getStdRandom, random)
 
+
+----------------------------------------------------------------------
+
+data Game = Game { handle :: Int,
+                   board :: Board } deriving Generic
 instance ToJSON Game
+
+data InternalGame = InternalGame Game Board
 
 
 main :: IO ()
 main = do 
- ref <- newIORef empty
+ ref <- newIORef Map.empty
  scotty 3000 $ do
   post "/game" $ do
-    b <- lift $ createValidStandardRandomBoard
-    gs <- liftIO $ readIORef ref
-    let g = Game (length gs) b
-    liftIO $ atomicModifyIORef' ref $ \s -> (s |> g, ())
-    json $ g
+    let s = emptyStandardBoard
+    i <- liftIO $ getStdRandom random
+    let g = Game i s
+    b <- liftIO $ createValidStandardRandomBoard
+    let ig = InternalGame g b
+    liftIO $ atomicModifyIORef' ref $ \gs -> (Map.insert i ig gs, ())
+    json g
   get "/game/:id" $ do
     i <- param "id"
     gs <- liftIO $ readIORef ref
-    let mgame = gs !? i
-    case mgame of
-      Just game -> json game
+    let migame = Map.lookup i gs
+    case migame of
+      Just (InternalGame game _) -> json game
       Nothing   -> status notFound404
-    
