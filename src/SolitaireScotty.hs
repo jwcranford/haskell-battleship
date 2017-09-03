12ac2f05abb90b8c22ce34924a6a14ea485e2ae9
Jonathan W Cranford <jonathan.w.cranford@gmail.com>
2017-09-02 20:09:52 -0600

    Added PUT /game/:id/shoot/:row/:column
    Now uses positive game ID < 100000

--- src/SolitaireScotty.hs
+++ src/SolitaireScotty.hs
@@ -3,10 +3,10 @@
 
 import Battleship (Board,
                    createValidStandardRandomBoard,
-                   emptyStandardBoard)
+                   emptyStandardBoard, shoot, applyShotResults, shipsSunk)
 
-import Data.IORef (newIORef, atomicModifyIORef', readIORef)
-import qualified Data.Map.Strict as Map (empty, lookup, insert)
+import Data.IORef (newIORef, atomicModifyIORef', readIORef, IORef)
+import qualified Data.Map.Strict as Map (empty, lookup, insert, Map)
 
 -- ghc
 import GHC.Generics (Generic)
@@ -15,14 +15,14 @@ import GHC.Generics (Generic)
 import Control.Monad.Trans (liftIO)
 
 -- scotty
-import Web.Scotty (scotty, post, get, param, json, status)
+import Web.Scotty (scotty, post, put, get, param, json, status)
 import Network.HTTP.Types.Status (notFound404)
 
 -- aeson
-import Data.Aeson (ToJSON)
+import Data.Aeson (ToJSON, FromJSON)
 
 -- random
-import System.Random (getStdRandom, random)
+import System.Random (randomRIO)
 
 
 ----------------------------------------------------------------------
@@ -40,7 +40,7 @@ main = do
  scotty 3000 $ do
   post "/game" $ do
     let s = emptyStandardBoard
-    i <- liftIO $ getStdRandom random
+    i <- liftIO $ randomRIO (0,100000)
     let g = Game i s
     b <- liftIO $ createValidStandardRandomBoard
     let ig = InternalGame g b
@@ -53,3 +53,22 @@ main = do
     case migame of
       Just (InternalGame game _) -> json game
       Nothing   -> status notFound404
+  put "/game/:id/shoot/:row/:column" $ do
+    i <- param "id"
+    r <- param "row"
+    c <- param "column"
+--    liftIO $ print (r,c)
+    gs <- liftIO $ readIORef ref
+    let migame = Map.lookup i gs
+    case migame of
+      Just (InternalGame game rb) ->
+        let (hit, rb') = shoot (r,c) rb
+            sb' = applyShotResults ((r,c), hit) (shipsSunk rb') (board game)
+            game' = Game i sb'
+            ig' = InternalGame game' rb'
+        -- update game
+        in do
+          liftIO $ atomicModifyIORef' ref $ \gs2 -> (Map.insert i ig' gs2, ())
+          json game'
+      Nothing   -> status notFound404
+    
